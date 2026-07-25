0ba30d087d3a247970da3dc24eb5f5f437301c25
Rohit22t <155294877+Rohit22t@users.noreply.github.com>
2026-07-26 05:27:34 +0800

    Update serverless.js

--- serverless.js
+++ serverless.js
@@ -1,10 +1,19 @@
 const { getRouter } = require('stremio-addon-sdk');
-const addonInterface = require('./addon.js');
-const router = getRouter(addonInterface);
+const addon = require('./addon.js');
 
+// Get the router from the addon
+const router = getRouter(addon);
+
+// Export as serverless function
 module.exports = function(req, res) {
-    router(req, res, function() {
+    router(req, res, function(err) {
+        if (err) {
+            console.error('Error:', err);
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+            return;
+        }
         res.statusCode = 404;
-        res.end();
+        res.end('Not Found');
     });
 };
